1d8f46e7afe3dfa1c8ecd05bd3f6f8ff9dff91db
David Young-chan Kay <davidykay@gmail.com>
2017-11-14 19:20:31 -0800

    Initial import

---
+++ test/sente_interceptor/core_test.clj
@@ -0,0 +1,7 @@
+(ns sente-interceptor.core-test
+  (:require [clojure.test :refer :all]
+            [sente-interceptor.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
